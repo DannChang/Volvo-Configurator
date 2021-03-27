2880d482407f07df22506bf1e06c9748afbce93d
dmagma <dmagma@hotmail.com>
2021-03-26 19:44:22 -0700

    Add tweening packages

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'dat.gui'
 import { gsap } from 'gsap'
+import * as TWEEN from '@tweenjs/tween.js'
 
 /**
  * Loaders
